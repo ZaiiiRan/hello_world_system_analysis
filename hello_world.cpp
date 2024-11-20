784051c9e1c701137cc746bafc3206f5ba124cc7
ZaiiiRan <a1b075753@gmail.com>
2024-11-20 20:39:17 +0300

    Fix: removed 'using namespace std; .'

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std; .
 
 int main() {
-    string name;
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "\nHello world from " << name << endl;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "\nHello world from " << name << std::endl;
     return 0;
 }
