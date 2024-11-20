089bc4e7e83028f71b5123ccfeb3cde2bad99a13
ZaiiiRan <a1b075753@gmail.com>
2024-11-20 20:55:59 +0300

    Docs: added comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 int main() {
+    // ask user's name
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
+
+    // print personalized heelo world message
     std::cout << "\nHello world from " << name << std::endl;
     return 0;
 }
